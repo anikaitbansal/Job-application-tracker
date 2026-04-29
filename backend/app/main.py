f9fcd7f96b76c398196e24233d2b70956186a000
anikaitbansal <anikaitbansal@gmail.com>
2026-04-29 13:21:31 +0530

    Day 4: Built secure user registration system

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 
-from backend.app.database import SessionLocal
+from backend.app.database import SessionLocal, get_db
+from backend.app.models.user import User
+from backend.app.schemas.user import UserCreate, UserResponse
+from backend.app.security import hash_password
+
+
 app = FastAPI()
 
 
@@ -19,3 +25,23 @@ def db_test():
         return {"message": "Database connection successful"}
     finally:
         db.close()
+
+
+@app.post("/register", response_model=UserResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        hashed_password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+
+    class Config:
+        from_attributes = True
